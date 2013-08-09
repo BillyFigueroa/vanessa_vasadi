c9e1655c9d1fdc7a6b3143103947cb3b63697031
Billy Figueroa <figuer25@gmail.com>
2013-08-08 20:29:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VanessaVasadi::Application.config.secret_token = '93fdbdd631ad259998bba22cbf1c68a7ae440bdb2c23bbbe39d7f1eee0fa076703f16fb7af67ae1d123bd334dca1b9e3f0fd038089d41c01f450374b75938cbb'
